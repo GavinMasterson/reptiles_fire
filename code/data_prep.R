db14405b30471b877387fed43f7b22ee8b024d61
GavinMasterson <gavinmasterson@gmail.com>
2020-10-18 23:24:46 +0200

    lots of data cleaning and normalising (separating dfs)

--- code/data_prep.R
+++ code/data_prep.R
@@ -22,8 +22,46 @@ clean_data <- raw_data %>%
                      "trap_position"),
                    sep = "[-]",
                    fill = "right") %>%
-          mutate_if(is.character,as.factor)
+          write_csv(here("/data/clean_data.csv"))
 
 # view a summary of the data
-skimr::skim(clean_data)
-write_csv(clean_data, here("/data/clean_data.csv"))
+clean_data %>%
+  mutate_if(is.character, as.factor) %>%
+  skimr::skim()
+
+#######################################################################################################
+# normalize the data by creating additional dataframes
+# specific to each observational unit e.g. captures, trap_sites, phase, species taxonomy etc.
+
+# Trap site
+trapsite_data <- clean_data %>%
+  select(trap_site, block, lat, long) %>%
+  distinct() %>%
+  drop_na() %>%
+  write_csv(here("/data/trapsite_data.csv"))
+
+# Project phase
+
+phase_data <- clean_data %>%
+  select(date, phase) %>%
+  distinct() %>%
+  arrange(date) %>%
+  write_csv(here("/data/phase_data.csv"))
+
+# Capture data
+capture_data <- clean_data %>%
+  select(-phase, -block, -treatment, -lat, -long) %>%
+  drop_na(species) %>%
+  write_csv(here("/data/capture_data.csv"))
+
+# Species data (correct taxonomy using {taxadb}, common names, snake/lizard)
+
+clean_data %>%
+  select(arm, trap_type, trap_position) %>%
+  filter(trap_type == "funnel") %>%
+  View()
+
+# To do:
+# Need to figure out how to deal with treatment and the dates the traps weren't checked.
+# Plotting time series of days using a dataset of dates checked and not-checked
+# would allow for visualising the zero detection days from the not-checked days.
