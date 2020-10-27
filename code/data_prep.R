41804952c9754c4a791002ca31c71be555f90eea
GavinMasterson <gavinmasterson@gmail.com>
2020-10-27 23:20:55 +0200

    finished the species_data file code and streamlined the code

--- code/data_prep.R
+++ code/data_prep.R
@@ -1,11 +1,11 @@
 # Load packages that are used more than once
 
-library(here)
 library(tidyverse)
 library(readxl)
 library(lubridate)
 library(skimr)
 library(taxadb)
+library(here)
 
 # import raw data as captured during project
 raw_data <- read_xlsx(path = here("/data/all_data.xlsx"),
@@ -36,6 +36,8 @@ clean_data %>%
 # normalise the database for this project by creating additional dataframes
 # specific to each observational unit e.g. captures, trap_sites, phase, species taxonomy etc.
 
+clean_data <- read_csv(here("/data/clean_data.csv"))
+
 # Trap site
 trapsite_data <- clean_data %>%
   select(trap_site, block, lat, long) %>%
@@ -61,34 +63,22 @@ species_data <- clean_data %>%
   select(species) %>%
   distinct() %>%
   drop_na() %>%
-  arrange(species)
-
-# get ITIS numbers for the
-td_create("itis", dbdir = here("/data"))
-
-# The filter_name function only works on a character vector so we convert it for this function
-species_names <- as.character(species_data$species)
-
-species_names %>%
-  fuzzy_filter(by = "scientificName",
-               match = "contains") %>%
-  filter_id(c(taxonID), type = "acceptedNameUsageID") %>%
+  arrange(species) %>%
+  as.matrix() %>%
+  as.character() %>%
+  # The filter_name function only works on a character vector so we convert it for this function
+  filter_name() %>%
   select(taxonomy_2006 = input,
-         acceptedNameUsageID,
+         taxonomicStatus,
          genus_ITIS = genus,
          specificEpithet_ITIS = specificEpithet,
-         taxonomicStatus_ITIS = taxonomicStatus,
-         vernacularName_ITIS = vernacularName)
-
-%>%
-  select(taxonID,
-         scientificName,
-         taxonRank,
-         taxonomicStatus,
-         class)
-
-species_data%>%
+         vernacularName,
+         everything(),
+         -sort,
+         -scientificName) %>%
   write_csv(here("/data/species_data.csv"))
 
 # TODO: Need to figure out how to deal with treatment and the dates the traps weren't checked.
 # Plotting time series of days using a dataset of dates checked and not-checked would allow for visualising the zero detection days from the not-checked days.
+
+
